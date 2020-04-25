cacd069f528d1dad36d4552294f92a97e906404a
MH Rilwan <rilwanfit@gmail.com>
2020-04-25 16:47:22 +0200

    Add twig template support

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,6 +23,15 @@ class QuestionController
      */
     public function show()
     {
-        return new Response('Show question!');
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => 'Show question?',
+            'answers' => $answers
+        ]);
     }
 }
