84d9c4fae746c90b71cde28ea64a440bb75c8b62
MH Rilwan <rilwanfit@gmail.com>
2020-05-13 18:40:44 +0200

    Add phpunit support

---
+++ tests/Unit/Entity/AuthorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class AuthorTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
